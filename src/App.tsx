e17b12ae659e789609beb57288eb1d099ccbc833
melion11 <gphooenix@gmail.com>
2023-04-25 18:47:53 +0300

    test commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     const tasks: TaskType[] = [
         {id: 1, title: 'HTML&CSS', isDone: true},
         {id: 2, title: 'JS/ES6/TS', isDone: true},
-        {id: 3, title: 'React', isDone: false},
+        {id: 3, title: 'React/Redux', isDone: false},
     ]
 
     return (
